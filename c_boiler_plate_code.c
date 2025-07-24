72020650510f7e851d7f3de1e879417977a7212f
viktorexe <developer.viktorexe@gmail.com>
2025-07-24 21:58:53 +0530

    c-boiler-plat-code

---
+++ c_boiler_plate_code.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main() {
+
+    return 0;
+}
+// This is a c language boiler plate code, a.k.a the main code you will use for every other c file
