bb67db544c5d03f651e6da06b58187e07a21a342
viktorexe <developer.viktorexe@gmail.com>
2025-08-04 21:25:59 +0530

    re-commit

---
+++ 1.1 Beginning/2. code_explanation.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf('Hello World!');
+    return 0;
+}
+/* Explanation for the above code
+ * # include <stdio.h>, it is a standard header file needed to run basic c programs
+ * int main () is the main function inside which is the print statement
+ */
