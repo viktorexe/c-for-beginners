35939d76e874e642ad2f415cf1162cd1a01ad064
viktorexe <developer.viktorexe@gmail.com>
2025-08-04 20:49:40 +0530

    renamed + commit

---
+++ 1) Basics of C Language/basic_information.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main() {
+    printf("C Language"); // This printf statement is used to print a line in C Language
+    return 0;
+}
+
+// This is a c language boiler plate code, a.k.a the main code you will use for every other c file
+// How does this code works?
+// First this code is  taken by compiler and translated in such a way for CPU to understand
+// (Converts to binary)
