7f6761dba601ab0b607d907aeac7707bd6c6dfaf
viktorexe <developer.viktorexe@gmail.com>
2025-08-06 22:46:11 +0530

    Update 2_variables_info.c

--- 1.1_Beginning/2_variables_info.c
+++ 1.1_Beginning/2_variables_info.c
@@ -2,7 +2,20 @@
 
 int main() {
     int a = 1;
-    printf("%d", a);
+    printf("%d\n", a);
+    float b = 12.22;
+    printf("%f\n", b);
+    double c = 12.7287937193131211;
+    printf("%lf\n", c);
+    char d;
+    d = 'a'; // Char variable stores only 1 letter
+    printf("%c\n", d);
     return 0;
 }
 
+/* Bytes consumed by each datatype
+ * int = 2 byte
+ * float = 4 byte
+ * double = 8 byte
+ * char = 1 byte
+ */
