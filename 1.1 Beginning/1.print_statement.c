919b9479a2831be95e227a6ecad64331d9374eb3
viktorexe <developer.viktorexe@gmail.com>
2025-08-06 12:51:52 +0530

    06.08.2025 initial commit

--- 1.1 Beginning/1.print_statement.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf('Hello World!'); // Statement to print Hello World
-    return 0;
-}
---
+++ 1.1_Beginning/1_print_statement.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+
+int main() {
+    printf("Hello World");
+    return 0;
+}
+
+
+/* Explanation for this program
+ * # include <stdio.h>
+This tells the compiler to include a standard library called stdio.h.
+stdio means Standard Input/Output.
+It contains useful functions like printf, scanf, etc.
+
+* int main ()
+This is the main function. Every C program starts executing from here.
+int means this function will return an integer value (usually 0 at the end).
+{ marks the start of the block of code inside the main function.
+
+* printf("Hello World");
+This line prints text on the screen.
+printf() is a function from stdio.h.
+"Hello World" is the message that gets printed.
+
+* return 0;
+Ends the main() function.
+Returns 0 to the operating system, which means the program ran successfully.
+*/
